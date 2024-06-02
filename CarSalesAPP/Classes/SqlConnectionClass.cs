d76773720f8e57509f0e9616ba0c98f3afca04a4
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-06-02 18:03:37 +0300

    Sql Connection Provided and Page Design Added via Bootstrap

---
+++ CarSalesAPP/Classes/SqlConnectionClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace CarSalesAPP.Classes
+{
+    public class SqlConnectionClass
+    {
+        public static SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8R1K9JN\\BATINS;Initial Catalog=CarSalesAPP;Integrated Security=True;Encrypt=False");
+
+        public static void CheckConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+    }
+}
