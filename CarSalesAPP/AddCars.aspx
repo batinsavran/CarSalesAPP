985f088cbfc57c5b3c93aa075695c8eb7834deff
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-06-08 14:57:16 +0300

    Vehicle Advertisement Addition Page was created

---
+++ CarSalesAPP/AddCars.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AddCars.aspx.cs" Inherits="CarSalesAPP.AddCars" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+        <table class="w-100">
+            <tr>
+                <td style="width: 181px">Araba Markasını Seçiniz : </td>
+                <td>
+                    <asp:DropDownList ID="DropDownList1" runat="server" Height="16px" Width="180px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px">Araba Modelini Giriniz :</td>
+                <td>
+                    <asp:TextBox ID="tbModel" runat="server" Width="172px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px; height: 27px">Araba Fotoğraf Linki : </td>
+                <td style="height: 27px">
+                    <asp:TextBox ID="tbPhoto" runat="server" Width="174px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px">Araba Yakıt Tipi Giriniz : </td>
+                <td>
+                    <asp:TextBox ID="tbFuelType" runat="server" Width="173px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px">Açıklama Giriniz :</td>
+                <td>
+                    <asp:TextBox ID="tbDescription" runat="server" Height="131px" TextMode="MultiLine" Width="180px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px; height: 21px">Araç Satıcısı :</td>
+                <td style="height: 21px">
+                    <asp:TextBox ID="tbSeller" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px; height: 21px">İletişim Numarası : </td>
+                <td style="height: 21px">
+                    <asp:TextBox ID="tbContact" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px">Araç Fiyatı : </td>
+                <td>
+                    <asp:TextBox ID="tbPrice" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 181px">&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="İlan Oluştur" Width="136px" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</asp:Content>
---
+++ CarSalesAPP/AddCars.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik olarak oluşturulmuş>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler hatalı davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik olarak oluşturulmuş>
+//------------------------------------------------------------------------------
+
+namespace CarSalesAPP
+{
+
+
+    public partial class AddCars
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// DropDownList1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// tbModel denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbModel;
+
+        /// <summary>
+        /// tbPhoto denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbPhoto;
+
+        /// <summary>
+        /// tbFuelType denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbFuelType;
+
+        /// <summary>
+        /// tbDescription denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbDescription;
+
+        /// <summary>
+        /// tbSeller denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbSeller;
+
+        /// <summary>
+        /// tbContact denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbContact;
+
+        /// <summary>
+        /// tbPrice denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbPrice;
+
+        /// <summary>
+        /// Button1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
