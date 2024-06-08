9f979b7bed6081ff0d8cc95164cb7fb5a5f4f824
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-06-09 00:52:41 +0300

    Admin Users Page is completed. All users are listed with their information.

---
+++ CarSalesAPP/AUsers.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AUsers.aspx.cs" Inherits="CarSalesAPP.AUsers" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+        <div style="margin-left: 80px">
+            <asp:DataGrid ID="DataGrid1" runat="server" CellPadding="2" ForeColor="#333333" GridLines="None" AutoGenerateColumns="False" Height="450px" Width="1350px">
+                <Columns>
+                    <asp:BoundColumn DataField="UserID" HeaderText="User ID" />
+                    <asp:BoundColumn DataField="UserMail" HeaderText="User Mail" />
+                    <asp:BoundColumn DataField="UserPassword" HeaderText="User Password" />
+                </Columns>
+                <AlternatingItemStyle BackColor="White" />
+                <EditItemStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <ItemStyle BackColor="#E3EAEB" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <SelectedItemStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+            </asp:DataGrid>
+        </div>
+    </form>
+</asp:Content>
---
+++ CarSalesAPP/AUsers.aspx.cs
@@ -0,0 +1,53 @@
+﻿using CarSalesAPP.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CarSalesAPP
+{
+    public partial class AUsers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadUserData();
+            }
+        }
+
+        private void LoadUserData()
+        {
+            try
+            {
+                SqlConnectionClass.CheckConnection();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT UserID, UserMail, UserPassword FROM TableUser", SqlConnectionClass.connection))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        DataGrid1.DataSource = dt;
+                        DataGrid1.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Veritabanı hatası: " + ex.Message);
+            }
+            finally
+            {
+                if (SqlConnectionClass.connection.State == ConnectionState.Open)
+                {
+                    SqlConnectionClass.connection.Close();
+                }
+            }
+        }
+    }
+}
