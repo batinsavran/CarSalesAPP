eeea676527afc933a981e100549f4533c4d1b4f0
Batın Gökçe Savran <batinsavran00@gmail.com>
2024-06-08 20:59:09 +0300

    Register Page Completed All

---
+++ CarSalesAPP/Register.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="CarSalesAPP.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+        <asp:TextBox ID="tbMail" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="tbPassword" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnRegister" runat="server" OnClick="btnRegister_Click" style="height: 29px" Text="Kayıt Ol" />
+    </form>
+</asp:Content>
---
+++ CarSalesAPP/Register.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using CarSalesAPP.Classes;
+
+namespace CarSalesAPP
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("Insert into TableUser (UserMail, UserPassword) values (@pmail,@ppassword)", SqlConnectionClass.connection);
+            SqlConnectionClass.CheckConnection();
+
+            sqlCommand.Parameters.AddWithValue("@pmail", tbMail.Text);
+            sqlCommand.Parameters.AddWithValue("@ppassword", tbPassword.Text);
+            sqlCommand.ExecuteNonQuery();
+        }
+    }
+}
